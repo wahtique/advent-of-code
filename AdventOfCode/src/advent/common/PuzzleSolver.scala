1d416d01325e5a456bdfad99a100e8f729b5331c
vealphaw <william.veal-phan@kelkoogroup.com>
2021-12-02 09:51:52 +0100

    refactor: make everything more cats-effect

--- AdventOfCode/src/advent/common/PuzzleSolver.scala
+++ AdventOfCode/src/advent/common/PuzzleSolver.scala
@@ -4,26 +4,35 @@ import cats.effect._
 import cats.effect.implicits._
 import scala.io.Source.fromFile
 import scala.io.BufferedSource
-import cats.effect.unsafe.implicits._
 
-abstract class Puzzle[I, O](val inputFileName: String):
+
+trait PuzzleSolver[I, O]:
+
+  val inputFileName: String
 
   // to define for each puzzle
+
   val parse: BufferedSource => I
   val solutionPart1: I => O
   val solutionPart2: I => O
 
   // common to each puzzle
+
   lazy val inputFile: Resource[IO, BufferedSource] =
     Resource.fromAutoCloseable(IO(fromFile(inputFileName)))
-  def solve1: O = inputFile
+
+  def solve1: IO[O] = inputFile
     .use(source => IO(parse(source)))
     .map(solutionPart1)
-    .unsafeRunSync()
-  def solve2: O = inputFile
+
+  def solve2: IO[O] = inputFile
     .use(source => IO(parse(source)))
     .map(solutionPart2)
-    .unsafeRunSync()
+
   def printSolutions =
-    println(s"Solution part 1: $solve1")
-    println(s"Solution part 2: $solve2")
+    for 
+      solution1 <- solve1
+      _ <- IO(println(solution1))
+      solution2 <- solve2
+      _ <- IO(println(solution2))
+    yield ()
--- AdventOfCode/test/src/advent/y2021/Day01Suite.scala
+++ AdventOfCode/test/src/advent/y2021/Day01Suite.scala
@@ -1,7 +1,10 @@
 package advent.y2021
 
-class Day01Suite extends munit.FunSuite:
-  val puzzle = Day01("AdventOfCode/test/resources/y2021/day01.txt")
-  test("part1")(assertEquals(puzzle.solve1, 7))
-  test("part2")(assertEquals(puzzle.solve2, 5))
+import munit.CatsEffectSuite
+import cats.effect.IO
+
+class Day01Suite extends CatsEffectSuite:
+  val solver = Day01("AdventOfCode/test/resources/y2021/day01.txt")
+  test("part1")(solver.solve1.flatMap(result => IO(assertEquals(result, 7))))
+  test("part2")(solver.solve2.flatMap(result => IO(assertEquals(result, 5))))
   
